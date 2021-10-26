2ee7b7bfcfb40867bc0f9fa5157c00dcd4d24d04
Pierre Petitpas <petitpaspierre@live.com>
2021-10-26 19:40:13 +0200

    change connection

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://localhost:27017/myBandsDB', { useNewUrlParser: true, useUnifiedTopology: true });
-//mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect('mongodb://localhost:27017/myBandsDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 app.use((err, req, res, next) => {
