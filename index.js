d00b40f5a6afc7d68cae783d8c6493fd417c96f3
Pierre Petitpas <petitpaspierre@live.com>
2021-11-02 14:03:36 +0100

    fixing cors

--- index.js
+++ index.js
@@ -34,18 +34,6 @@ const cors = require('cors');
 app.use(cors());
 let auth = require('./auth')(app);
 
-let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234'];
-
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message ), false);
-    }
-    return callback(null, true);
-  }
-}));
 
 app.get('/', (req, res) => {
     res.send('Welcome to my metal bands app!');
